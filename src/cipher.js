2a699e6b070a5cfd7131e7d0a64eebc08992c648
Estudiante <yudith.cumba@gmail.com>
2020-02-14 08:14:04 -0500

    Subiendo js css

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,45 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {
+  /*cifrado cesar ->*/
+  let textchanged="";
+  let letterchanged ="";
+      for(let i=0; i<string.length; i++){
+           if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+              letterchanged= String.fromCharCode((string.charCodeAt(i) - 65 + offset) % 26+ 65);
+           }
+           else if(string.charCodeAt(i)==32){
+              letterchanged=" ";
+           }
+           else{
+              alert('Por favor, solo ingrese letras en mayúscula.');
+              break;
+           }
+           
+      textchanged = textchanged+letterchanged;
+      }
+      return textchanged;
+  },
+
+  decode: function (offset, string) {
+      
+  let textchanged="";
+  let letterchanged ="";
+  for(let i=0; i<string.length; i++){
+           if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+              letterchanged= String.fromCharCode((string.charCodeAt(i) - 65 - offset + 26 ) % 26+ 65);
+           }
+           else if(string.charCodeAt(i)==32){
+              letterchanged=" ";
+           }
+           else{
+              alert('Por favor, solo ingrese letras en mayúscula.');
+              break;
+           }
+           
+      textchanged = textchanged+letterchanged;
+      }
+      return textchanged;
+  }
 };
 
-export default cipher;
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
+/*console.log(cipher.encode());*/
 
-console.log(cipher);
+const btnCod = document.getElementById('btnCod');
+btnCod.addEventListener('click', () => {
+  let textOriginal=document.getElementById("textOriginal");
+  let movimientos=document.getElementById("movimientos");
+  document.getElementById("textChanged").innerHTML= cipher.encode(parseInt(movimientos.value),textOriginal.value);
+});
+
+const btnDecod= document.getElementById('btnDecod');
+
+btnDecod.addEventListener('click', () => {
+  let textOriginal=document.getElementById("textOriginal");
+  let movimientos=document.getElementById("movimientos");
+  document.getElementById("textChanged").innerHTML= cipher.decode(parseInt(movimientos.value),textOriginal.value);
+});
