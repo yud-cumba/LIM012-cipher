69047561335efadf894ce6b3c646bdce20325501
Estudiante <yudith.cumba@gmail.com>
2020-02-18 12:30:59 -0500

    holi

--- src/cipher.js
+++ src/cipher.js
@@ -6,6 +6,13 @@ const cipher = {
       for(let i=0; i<string.length; i++){
            if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
               letterchanged= String.fromCharCode((string.charCodeAt(i) - 65 + offset) % 26+ 65);
+              /*else if(string.charCodeAt(i)==32){
+               letterchanged=" ";
+            }
+            else{
+               alert('Por favor, solo ingrese letras en mayúscula.');
+               break;
+            }*/
            }
       textchanged = textchanged+letterchanged;
       }
